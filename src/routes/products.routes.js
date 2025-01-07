44559a15c063adc46989c94afc4503fe1980330c
zohaib240 <zohaibjs123@gmail.com>
2025-01-07 21:49:51 +0500

    update routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 
 // Add authentication middleware to protect these routes
 productRouter.post('/products', upload.single('image'), addProduct);
-productRouter.get("/products/:id",  allProducts);
+productRouter.get("/products",  allProducts);
 productRouter.get("/products/:id",  singleProduct);
 productRouter.delete("/products/:id", deleteProduct);  // Route updated with postId
 productRouter.put("/products/:id", upload.single("image"), updateProduct);
